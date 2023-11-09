c1fa0c613f8ab6095028193846ab36c755841a40
Park SangCheon <84065815+SangCheonP@users.noreply.github.com>
2023-11-10 04:06:01 +0900

    ADD
     - line 2

--- src/Welcome.java
+++ src/Welcome.java
@@ -4,6 +4,7 @@ public class Welcome {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Welcome to Shopping Mall!");
+		System.out.print("Welcome to Book Market!");
 	}
 
 }
