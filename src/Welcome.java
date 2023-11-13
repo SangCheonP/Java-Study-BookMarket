051bd4d9a3bec73b4246cc8459fc6f8589150573
Park SangCheon <84065815+SangCheonP@users.noreply.github.com>
2023-11-13 18:50:19 +0900

    CH3 end

--- src/Welcome.java
+++ src/Welcome.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
 
 public class Welcome {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("당신의 이름을 입력하세요 : ");
+		String userName = sc.next();
+		
+		System.out.print("연락처를 입력하세요 : ");
+		int userMobile = sc.nextInt();
 		
 		String greeting = "Welcome to Shopping Mall";
 		String tagline = "Welcome to Book Market";
@@ -15,6 +23,10 @@ public class Welcome {
 		System.out.println(" 3. 장바구니 비우기 \t6. 장바구니의 항목 삭제하기");
 		System.out.println(" 7. 영수증 표시하기 \t8. 종료");
 		System.out.println("***********************************************");
+		
+		System.out.print("메뉴 번호를 선택해주세요 ");
+		int n = sc.nextInt();
+		System.out.println(n + "번을 선택했습니다.");
 	}
 
 }
